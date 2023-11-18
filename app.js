9196721a647a37277ab7320c76464f536210898d
Jonathan <jonathanezequiel199728@gmail.com>
2023-11-18 20:12:33 -0300

    repaso de selectores y estilos basicos

---
+++ app.js
@@ -0,0 +1,5 @@
+const primerTopping = document.querySelector('.topping');
+
+primerTopping.style.backgroundColor = 'blue';
+primerTopping.style.color = 'green';
+primerTopping.style.textTransform = 'uppercase';
